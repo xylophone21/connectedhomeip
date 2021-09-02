3011997d6371b2ceffa718e537916d7780ce06ed
Hui Li <xylophone21@gmail.com>
2021-09-02 21:22:12 +0800

    fix log show error in CHIPTest

--- src/android/CHIPTest/app/src/main/java/com/tcl/chip/chiptest/MainActivity.kt
+++ src/android/CHIPTest/app/src/main/java/com/tcl/chip/chiptest/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.tcl.chip.chiptest
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
 import android.text.method.ScrollingMovementMethod
 import androidx.appcompat.app.AppCompatActivity
 import chip.platform.AndroidBLEManager
@@ -12,7 +15,19 @@ import com.tcl.chip.chiptest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val msgShowLog = 1
+
     private lateinit var binding: ActivityMainBinding
+    private val mainHandler: Handler = object : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg!!)
+            when (msg?.what) {
+                msgShowLog -> {
+                    binding.testLog.append(msg.obj.toString())
+                }
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,16 +37,23 @@ class MainActivity : AppCompatActivity() {
 
         binding.testLog.movementMethod = ScrollingMovementMethod();
         TestEngine.setListener { log ->
-            binding.testLog.append(log)
+            var msg = mainHandler.obtainMessage(msgShowLog, log)
+            mainHandler.sendMessage(msg)
         }
 
         binding.runButton.setOnClickListener {
-            var ret = TestEngine.runTest();
-            if (ret == 0) {
-                binding.testLog.append(getString(R.string.test_finished));
-            } else {
-                binding.testLog.append(getString(R.string.test_failed,ret));
-            }
+            binding.testLog.text = ""
+            Thread{
+                var ret = TestEngine.runTest();
+
+                var log:String = if (ret == 0) {
+                    getString(R.string.test_finished)
+                } else {
+                    getString(R.string.test_failed,ret)
+                }
+                var msg = mainHandler.obtainMessage(msgShowLog, log)
+                mainHandler.sendMessage(msg)
+            }.start()
         }
 
         AndroidChipPlatform.getInstance().bleManager = AndroidBLEManager();
